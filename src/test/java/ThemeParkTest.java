4748e96de24f165498a084873d123efae62d44d3
Patrick Cullen <patrick47cullen@gmail.com>
2018-05-07 21:45:23 +0100

    Intermediate version of Theme Park homework

--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -9,27 +9,43 @@ public class ThemeParkTest {
 
     ThemePark themePark;
     Waltzer waltzer;
+    Booster booster;
+    Rollercoaster rollercoaster;
     Customer customer1;
     Customer customer2;
     Customer customer3;
     Customer customer4;
 
     @Before
-    public void before() {
-
-
+    public void before(){
         customer1 = new Customer("Jimmy", 100, 28, 6, 44);
         customer2 = new Customer("Walter", 100, 24, 6, 54);
-        customer3 = new Customer("Malcolm", 100, 24, 6, 51);
+        customer3 = new Customer("Malcolm", 100, 24, 6, 151);
         customer4 = new Customer("Malcolm", 100, 24, 6, 51);
         ArrayList<Waltzer> waltzers = new ArrayList<>();
         waltzer = new Waltzer("Waltzer", 10, 12, 5, 60, 3);
         waltzers.add(waltzer);
+        ArrayList<Booster> boosters = new ArrayList<>();
+        booster = new Booster("Booster", 10, 12, 5, 60, 3);
+        boosters.add(booster);
+        ArrayList<Rollercoaster> rollercoasters = new ArrayList<>();
+        rollercoaster = new Rollercoaster("Rollercoaster", 10, 12, 5, 60, 3);
+        rollercoasters.add(rollercoaster);
+        themePark = new ThemePark(waltzers, boosters, rollercoasters);
+
     }
 
     @Test
     public void canCheckCustomerIntoWaltzer(){
         themePark.checkIntoWaltzer(customer1);
-        assertEquals(1, waltzer.getCustomerCount());
+        themePark.checkIntoWaltzer(customer2);
+        themePark.checkIntoWaltzer(customer3);
+//        themePark.checkIntoWaltzer(customer4);
+        assertEquals(2, waltzer.getCustomerCount());
+    }
+
+    @Test
+    public void getCustomerName(){
+        assertEquals("Jimmy", customer1.getName() );
     }
 }
